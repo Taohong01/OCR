90b9d3a7d69be6cd3977801fc48b8bbb706a648c
Taohong01 <taohong01@gmail.com>
2015-07-22 22:05:52 -0700

    several changes
    modify PCA and now it works
    modify ICA, but still problem
    add 2 tests

--- pcaocr.py
+++ pcaocr.py
@@ -11,7 +11,20 @@ from sklearn import metrics
 from sklearn.grid_search import GridSearchCV
 import pandas as pds
 from sklearn.metrics import classification_report
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, FastICA
+import matplotlib.pyplot as plt
+from scipy import signal
+
+
+import logging
+from time import time
+
+from numpy.random import RandomState
+
+
+from sklearn.datasets import fetch_olivetti_faces
+from sklearn.cluster import MiniBatchKMeans
+from sklearn import decomposition
 
 
 #class for loading file
@@ -97,49 +110,215 @@ class OCR(object):
         
         
     def myPCA(self):
-        #gray = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
-        gray = self.img
 
-        # Now we split the image to 5000 cells, each 20x20 size
-        cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
-        
-        # Make it into a Numpy array. It size will be (50,100,20,20)
-        x = np.array(cells)
-        
         # Now we prepare train_data and test_data.
-        train = x[:,:50].reshape(-1,400).astype(np.float32) # Size = (2500,400)
-        test = x[:,50:100].reshape(-1,400).astype(np.float32) # Size = (2500,400)
-        
-        nn =1000
+        train = np.array(self.train_cells)[:,:50].reshape(-1,400).astype(np.float32) # Size = (2500,400)
+        test = np.array(self.test_cells)[:,50:100].reshape(-1,400).astype(np.float32) # Size = (2500,400)
+        print 'looks good'
+        nn =0
         X = np.array(train[(nn):(nn+250),:])
         print 'data X\n', X.shape
-        num_componets =70
+        num_componets =10
         pca = PCA(n_components=num_componets)
         pca.fit(X)
         #print 'pca components', pca.components_
         #print 'pca explained variance ratio', pca.explained_variance_ratio_
         
         img0 = cv2.resize(pca.mean_.reshape(20,20), (0,0), fx =5, fy=5)
+        img0 = np.zeros(img0.shape)
+
         for ii in range(num_componets):
             img1 = cv2.resize(pca.components_[ii,:].reshape(20,20), (0,0), fx =5, fy=5)
             img0 = np.hstack((img0, img1))
-        #img1 = cv2.resize(pca.components_[0,:].reshape(20,20), (0,0), fx =10, fy=10)
-        #img2 = cv2.resize(pca.components_[1,:].reshape(20,20), (0,0), fx =10, fy=10)
-        #img3 = cv2.resize((pca.components_[0,:]+pca.components_[1,:]+pca.components_[2,:]+pca.components_[3,:]+pca.components_[4,:]).reshape(20,20), (0,0), fx =10, fy=10)
-        cv2.imshow('',img0)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+            #img1 = cv2.resize(pca.components_[0,:].reshape(20,20), (0,0), fx =10, fy=10)
+            #img2 = cv2.resize(pca.components_[1,:].reshape(20,20), (0,0), fx =10, fy=10)
+            #img3 = cv2.resize((pca.components_[0,:]+pca.components_[1,:]+pca.components_[2,:]+pca.components_[3,:]+pca.components_[4,:]).reshape(20,20), (0,0), fx =10, fy=10)
+        self.showImage(img0*10)
+
         print 'pca n_components', pca.n_components_
         
         print(sum(pca.explained_variance_ratio_))
+        
+        
+    def myICA(self):
+        # Now we prepare train_data and test_data.
+        train = np.array(self.train_cells)[:,:50].reshape(-1,400).astype(np.float32) # Size = (2500,400)
+        test = np.array(self.test_cells)[:,50:100].reshape(-1,400).astype(np.float32) # Size = (2500,400)
+        print 'looks good'
+        nn =500
+        X = np.array(train[(nn):(nn+250),:])
+        print 'data X shape is ', X.shape
+        num_componets = 400
+        ica = FastICA(n_components = num_componets)
+        newX = ica.fit(X).transform(X)
+
+      
+        img0 = cv2.resize(ica.mean_.reshape(20,20), (0,0), fx =5, fy=5)
+
+        for ii in range(10):
+            img1 = cv2.resize(ica.components_[ii,:].reshape(20,20), (0,0), fx =5, fy=5)
+            img0 = np.hstack((img0, img1))
+            #img1 = cv2.resize(pca.components_[0,:].reshape(20,20), (0,0), fx =10, fy=10)
+            #img2 = cv2.resize(pca.components_[1,:].reshape(20,20), (0,0), fx =10, fy=10)
+            #img3 = cv2.resize((pca.components_[0,:]+pca.components_[1,:]+pca.components_[2,:]+pca.components_[3,:]+pca.components_[4,:]).reshape(20,20), (0,0), fx =10, fy=10)
+        #self.showImage(img0)
+        
+        print 'newX shape is ', newX.shape
+        
+        img00 = cv2.resize(ica.mean_.reshape(20,20), (0,0), fx =5, fy=5)
+        img0 = img00
+        for ii in range(20):
+            img1 = cv2.resize(newX[ii,:].reshape(20,20), (0,0), fx =5, fy=5)
+            img0 = np.hstack((img0, img1))
+            #img1 = cv2.resize(pca.components_[0,:].reshape(20,20), (0,0), fx =10, fy=10)
+            #img2 = cv2.resize(pca.components_[1,:].reshape(20,20), (0,0), fx =10, fy=10)
+            #img3 = cv2.resize((pca.components_[0,:]+pca.components_[1,:]+pca.components_[2,:]+pca.components_[3,:]+pca.components_[4,:]).reshape(20,20), (0,0), fx =10, fy=10)
+        self.showImage(img0)   
+        
+    def test(self):
+        ###############################################################################
+        # Generate sample data
+        np.random.seed(0)
+        n_samples = 2000
+        time = np.linspace(0, 8, n_samples)
+        
+        s1 = np.sin(2 * time)  # Signal 1 : sinusoidal signal
+        s2 = np.sign(np.sin(3 * time))  # Signal 2 : square signal
+        s3 = signal.sawtooth(2 * np.pi * time)  # Signal 3: saw tooth signal
+        
+        S = np.c_[s1, s2, s3]
+        S += 0.2 * np.random.normal(size=S.shape)  # Add noise
+        
+        S /= S.std(axis=0)  # Standardize data
+        # Mix data
+        A = np.array([[1, 1, 1], [0.5, 2, 1.0], [1.5, 1.0, 2.0]])  # Mixing matrix
+        X = np.dot(S, A.T)  # Generate observations
+        
+        # Compute ICA
+        ica = FastICA(n_components=3)
+        S_ = ica.fit_transform(X)  # Reconstruct signals
+        A_ = ica.mixing_  # Get estimated mixing matrix
+        
+        # We can `prove` that the ICA model applies by reverting the unmixing.
+        assert np.allclose(X, np.dot(S_, A_.T) + ica.mean_)
+        
+        # For comparison, compute PCA
+        pca = PCA(n_components=3)
+        H = pca.fit_transform(X)  # Reconstruct signals based on orthogonal components
+        
+        ###############################################################################
+        # Plot results
+        
+        plt.figure()
+        
+        models = [X, S, S_, H]
+        names = ['Observations (mixed signal)',
+                 'True Sources',
+                 'ICA recovered signals', 
+                 'PCA recovered signals']
+        colors = ['red', 'steelblue', 'orange']
+        
+        for ii, (model, name) in enumerate(zip(models, names), 1):
+            plt.subplot(4, 1, ii)
+            plt.title(name)
+            for sig, color in zip(model.T, colors):
+                plt.plot(sig, color=color)
+        
+        plt.subplots_adjust(0.09, 0.04, 0.94, 0.94, 0.26, 0.46)
+        plt.show()   
+        
+        
+    def test2(self):
+        
+
+        # Display progress logs on stdout
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s %(levelname)s %(message)s')
+        n_row, n_col = 2, 3
+        n_components = n_row * n_col
+        image_shape = (64, 64)
+        rng = RandomState(0)
+        
+        ###############################################################################
+        # Load faces data
+        dataset = fetch_olivetti_faces(shuffle=True, random_state=rng)
+        faces = dataset.data
+        
+        n_samples, n_features = faces.shape
+        
+        # global centering
+        faces_centered = faces - faces.mean(axis=0)
+        
+        # local centering
+        faces_centered -= faces_centered.mean(axis=1).reshape(n_samples, -1)
+        
+        print("Dataset consists of %d faces" % n_samples)
+        print("each face has %d features" % n_features )
+        
+        ###############################################################################
+        def plot_gallery(title, images, n_col=n_col, n_row=n_row):
+            plt.figure(figsize=(2. * n_col, 2.26 * n_row))
+            plt.suptitle(title, size=16)
+            for i, comp in enumerate(images):
+                plt.subplot(n_row, n_col, i + 1)
+                vmax = max(comp.max(), -comp.min())
+                plt.imshow(comp.reshape(image_shape), cmap=plt.cm.gray,
+                           interpolation='nearest',
+                           vmin=-vmax, vmax=vmax)
+                plt.xticks(())
+                plt.yticks(())
+            plt.subplots_adjust(0.01, 0.05, 0.99, 0.93, 0.04, 0.)
+        
+        ###############################################################################
+        # List of the different estimators, whether to center and transpose the
+        # problem, and whether the transformer uses the clustering API.
+        estimators = [
+
+            ('Independent components - FastICA',
+             decomposition.FastICA(n_components=n_components, whiten=True),
+             True),
+             
+        ]
+        
+        
+        ###############################################################################
+        # Plot a sample of the input data
+        
+        plot_gallery("First centered Olivetti faces", faces_centered[:n_components])
+
+        ###############################################################################
+        # Do the estimation and plot it
+        
+        for name, estimator, center in estimators:
+            print("Extracting the top %d %s..." % (n_components, name))
+            t0 = time()
+            data = faces
+            if center:
+                data = faces_centered
+            estimator.fit(data)
+            train_time = (time() - t0)
+            print("done in %0.3fs" % train_time)
+            if hasattr(estimator, 'cluster_centers_'):
+                components_ = estimator.cluster_centers_
+            else:
+                components_ = estimator.components_
+            if hasattr(estimator, 'noise_variance_'):
+                plot_gallery("Pixelwise variance",
+                             estimator.noise_variance_.reshape(1, -1), n_col=1,
+                             n_row=1)
+            plot_gallery('%s - Train time %.1fs' % (name, train_time),
+                         components_[:n_components])
+        
+        plt.show()
                 
 def main():
     DigitOCR = OCR()
     DigitOCR.imageDicing()
     DigitOCR.dataDividing()
     #DigitOCR.showImage(DigitOCR.img)
-    DigitOCR.myPCA()
-        
+    #DigitOCR.myICA()
+    DigitOCR.test2()
+    
 if __name__ == '__main__':
     main()
     
